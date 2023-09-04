dd411cee0eb2530fbe75fb172d9500ddad73bc06
Vermunds <33235701+Vermunds@users.noreply.github.com>
2023-09-04 21:16:39 +0200

    Update for SFSE

--- PCH.h
+++ PCH.h
@@ -3,4 +3,7 @@
 #include <cstdint>
 #include <string>
 
+#define DLLEXPORT extern "C" [[maybe_unused]] __declspec(dllexport)
+#define SFSEAPI __cdecl
+
 using namespace std::literals;
--- main.cpp
+++ main.cpp
@@ -1,105 +1,48 @@
-#include <Windows.h>
-#include <Shlobj.h>
+#include <shlobj.h>
 
-#include <vector>
+#include <array>
+#include <chrono>
 #include <filesystem>
 #include <memory>
+#include <thread>
+#include <vector>
 
-#include <spdlog/spdlog.h>
 #include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/spdlog.h>
+
+#include "sfse/Hooks_Version.h"
+#include "sfse/PluginAPI.h"
+#include "sfse/PluginManager.h"
+#include "sfse_common/Relocation.h"
+#include "sfse_common/BranchTrampoline.h"
+#include "sfse_common/SafeWrite.h"
+#include "sfse_common/sfse_version.h"
 
 #include "version.h"
 
-namespace Relocation
-{
-	void SafeWriteBuf(uintptr_t addr, void* data, size_t len)
-	{
-		DWORD oldProtect;
-
-		VirtualProtect((void*)addr, len, PAGE_EXECUTE_READWRITE, &oldProtect);
-		memcpy((void*)addr, data, len);
-		VirtualProtect((void*)addr, len, oldProtect, &oldProtect);
-	}
-
-	
-	void SafeWrite8(uintptr_t addr, uint8_t data)
-	{
-		SafeWriteBuf(addr, &data, sizeof(data));
-	}
-
-	void SafeWrite16(uintptr_t addr, uint16_t data)
-	{
-		SafeWriteBuf(addr, &data, sizeof(data));
-	}
-
-	void SafeWrite32(uintptr_t addr, uint32_t data)
-	{
-		SafeWriteBuf(addr, &data, sizeof(data));
-	}
-
-	void SafeWrite64(uintptr_t addr, uint64_t data)
-	{
-		SafeWriteBuf(addr, &data, sizeof(data));
-	}
-
-static bool SafeWriteBranch_Internal(uintptr_t src, uintptr_t dst, uint8_t op)
-	{
-#pragma pack(push, 1)
-		struct Code
-		{
-			uint8_t op;
-			int32_t displ;
-		};
-#pragma pack(pop)
-
-		static_assert(sizeof(Code) == 5);
-
-		ptrdiff_t delta = dst - (src + sizeof(Code));
-		if ((delta < INT_MIN) || (delta > INT_MAX))
-		{
-			return false;
-		}
-		Code code;
-
-		code.op = op;
-		code.displ = delta;
-
-		SafeWriteBuf(src, &code, sizeof(code));
-
-		return true;
-	}
-
-	bool SafeWriteJump(uintptr_t src, uintptr_t dst)
-	{
-		return SafeWriteBranch_Internal(src, dst, 0xE9);
-	}
-
-	bool SafeWriteCall(uintptr_t src, uintptr_t dst)
-	{
-		return SafeWriteBranch_Internal(src, dst, 0xE8);
-	}
-}
+const RelocAddr<uintptr_t*> processButtonFuncAddr = 0x1F48B30;
+const RelocAddr<uintptr_t*> codeCaveAddr = 0xF9C22B;
+const RelocAddr<uintptr_t*> playerCharacterSingletonAddr = 0x5594D28;
 
 class ButtonEvent
 {
 public:
 	uint8_t unk[0x48];
-	float value;			// 48
-	float heldDownSecs;		// 4C
+	float value;         // 48
+	float heldDownSecs;  // 4C
 };
-static_assert(sizeof(ButtonEvent) == 0x50); // Not actual size
+static_assert(sizeof(ButtonEvent) == 0x50);  // Not actual size
 
 class PlayerCharacter
 {
 public:
-	
 	enum class Flag10E4 : uint8_t
 	{
 		kSprinting = 4
 	};
 
 	uint8_t unk[0x10E4];
-	Flag10E4 flag10E4;	// 10E4
+	Flag10E4 flag10E4;  // 10E4
 	uint8_t pad10E5;
 	uint8_t pad10E6;
 	uint8_t pad10E7;
@@ -124,40 +67,7 @@ bool SprintHandler_ProcessButton_IsDown_Hook(ButtonEvent* a_buttonEvent)
 	return false;
 }
 
-bool CheckModuleVersion(const char* moduleName, uint16_t a_major, uint16_t a_minor, uint16_t a_patch, uint16_t a_beta)
-{
-	DWORD handle;
-	DWORD versionSize = GetFileVersionInfoSizeA(moduleName, &handle);
-	if (versionSize == 0)
-	{
-		spdlog::critical("Unable to get version info!");
-		return false;
-	}
-
-	std::vector<char> versionInfoBuffer(versionSize);
-	if (!GetFileVersionInfoA(moduleName, 0, versionSize, versionInfoBuffer.data()))
-	{
-		spdlog::critical("Unable to retrieve version info!");
-		return false;
-	}
-
-	VS_FIXEDFILEINFO* fileInfo;
-	UINT fileInfoSize;
-	if (!VerQueryValueA(versionInfoBuffer.data(), "\\", reinterpret_cast<void**>(&fileInfo), &fileInfoSize))
-	{
-		spdlog::critical("Unable to query version info!");
-		return false;
-	}
-
-	uint16_t moduleMajor = HIWORD(fileInfo->dwFileVersionMS);
-	uint16_t moduleMinor = LOWORD(fileInfo->dwFileVersionMS);
-	uint16_t moduleBuild = HIWORD(fileInfo->dwFileVersionLS);
-	uint16_t moduleRevision = LOWORD(fileInfo->dwFileVersionLS);
-
-	return (moduleMajor == a_major && moduleMinor == a_minor && moduleBuild == a_patch && moduleRevision == a_beta);
-}
-
-std::optional<std::filesystem::path> log_directory()
+std::optional<std::filesystem::path> LogDirectory()
 {
 	wchar_t* buffer{ nullptr };
 	const auto result = SHGetKnownFolderPath(::FOLDERID_Documents, KNOWN_FOLDER_FLAG::KF_FLAG_DEFAULT, nullptr, std::addressof(buffer));
@@ -168,54 +78,107 @@ std::optional<std::filesystem::path> log_directory()
 	}
 
 	std::filesystem::path path = knownPath.get();
-	path /= "My Games/Starfield/SFSE"sv;
+	path /= "My Games/Starfield/SFSE/Logs"sv;
 	return path;
 }
 
-bool APIENTRY DllMain(HMODULE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
+constexpr std::array<char, 256> CreateStringArray(const char* a_str) noexcept
 {
-	if (fdwReason == DLL_PROCESS_ATTACH)
+	std::array<char, 256> result{};
+	for (size_t i = 0; i < result.size() && a_str[i] != '\0'; ++i)
 	{
-		// Create logger
-		auto path = log_directory().value() / std::filesystem::path("ClassicSprintingStarfield.log"s);
-		auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path.string(), true);
-		auto log = std::make_shared<spdlog::logger>("global log", std::move(sink));
-
-		log->set_level(spdlog::level::trace);
-		log->flush_on(spdlog::level::trace);
-
-		spdlog::set_default_logger(std::move(log));
-		spdlog::set_pattern("%g(%#): [%^%l%$] %v", spdlog::pattern_time_type::local);
-
-		spdlog::info("Classic Sprinting Starfield v{} -({})", Version::NAME, __TIMESTAMP__);
-
-		// Check version info
-		const char* moduleName = "Starfield.exe";
-		if (!CheckModuleVersion(moduleName, 1, 7, 23, 0))
-		{
-			spdlog::critical("Unsupported runtime version!");
-			return false;
-		}
-
-		HMODULE targetModule = GetModuleHandleA(moduleName);
-		if (targetModule == nullptr)
-		{
-			spdlog::critical("Target module not found!");
-			return false;
-		}
-
-		// Install hook
-		uintptr_t processButtonFuncAddr = reinterpret_cast<uintptr_t>(targetModule) + 0x1F48B30;
-		uintptr_t hookAddr = processButtonFuncAddr + 0xC;
-		uintptr_t codeCaveAddr = reinterpret_cast<uintptr_t>(targetModule) + 0xF9C22B; // A large-ish unused spot between some random functions
-		g_playerCharacter = reinterpret_cast<PlayerCharacter**>(reinterpret_cast<uintptr_t>(targetModule) + 0x5594D28);
-
-		Relocation::SafeWriteJump(hookAddr, (uintptr_t)codeCaveAddr);
-
-		Relocation::SafeWrite16(codeCaveAddr, 0xB848);  // mov rax
-		Relocation::SafeWrite64(codeCaveAddr + 0X2, (uintptr_t)SprintHandler_ProcessButton_IsDown_Hook);
-		Relocation::SafeWrite16(codeCaveAddr + 0xA, 0xD0FF); // call rax
-		Relocation::SafeWriteJump(codeCaveAddr + 0xC, (uintptr_t)hookAddr + 0x5);  // jmp
+		result[i] = a_str[i];
 	}
+	return result;
+}
+
+DLLEXPORT constinit auto SFSEPlugin_Version = []() noexcept {
+	SFSEPluginVersionData data{};
+
+	data.dataVersion = SFSEPluginVersionData::kVersion;
+	data.pluginVersion = Version::MAJOR;
+
+	constexpr auto name = CreateStringArray(Version::PROJECT.data());
+	std::copy(name.begin(), name.end(), data.name);
+
+	constexpr auto author = CreateStringArray("Vermunds");
+	std::copy(author.begin(), author.end(), data.author);
+
+	data.addressIndependence = 0;
+	data.structureIndependence = SFSEPluginVersionData::kStructureIndependence_InitialLayout;
+
+	data.compatibleVersions[0] = RUNTIME_VERSION_1_7_23;
+	data.compatibleVersions[1] = 0;
+
+	data.seVersionRequired = 0;
+	data.reservedNonBreaking = 0;
+	data.reservedBreaking = 0;
+
+	return data;
+}();
+
+DLLEXPORT bool SFSEAPI SFSEPlugin_Load(SFSEInterface* a_sfse)
+{
+#ifndef NDEBUG
+	constexpr int waitForDebuggerTimeoutSecs = 5;
+
+	auto startTime = std::chrono::steady_clock::now();
+	while (!IsDebuggerPresent())
+	{
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+		auto currentTime = std::chrono::steady_clock::now();
+		if (std::chrono::duration_cast<std::chrono::seconds>(currentTime - startTime).count() >= waitForDebuggerTimeoutSecs)
+		{
+			break;
+		}
+	}
+#endif
+
+	// Check for broken install
+	std::filesystem::path asiFilePath = std::filesystem::current_path() / "plugins" / "ClassicSprintingStarfield.asi";
+	if (std::filesystem::exists(asiFilePath))
+	{
+		const char* errorMessage =
+			"An old version of Classic Sprinting for Starfield is installed.\n\n"
+			"Please delete \"ClassicSprintingStarfield.asi\" in the plugins folder, inside the root directory of the game.\n\n"
+			"Until this file is not deleted, the new version will be disabled.";
+		MessageBoxA(NULL, errorMessage, "Classic Sprinting for Starfield", MB_OK | MB_ICONERROR);
+
+		return false;
+	}
+
+	if (!g_branchTrampoline.create(1 << 4))
+	{
+		spdlog::critical("Couldn't create branch trampoline. The mod is disabled.");
+		return false;
+	}
+
+	// Create logger
+	auto path = LogDirectory().value() / std::filesystem::path("ClassicSprintingStarfield.log"s);
+	auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path.string(), true);
+	auto log = std::make_shared<spdlog::logger>("global log", std::move(sink));
+
+	log->set_level(spdlog::level::trace);
+	log->flush_on(spdlog::level::trace);
+
+	spdlog::set_default_logger(std::move(log));
+	spdlog::set_pattern("%g(%#): [%^%l%$] %v", spdlog::pattern_time_type::local);
+
+	spdlog::info("Classic Sprinting Starfield v{} -({})", Version::NAME, __TIMESTAMP__);
+
+	// Install hook
+	uintptr_t hookAddr = processButtonFuncAddr.getUIntPtr() + 0xC;
+	g_playerCharacter = reinterpret_cast<PlayerCharacter**>(playerCharacterSingletonAddr.getUIntPtr());
+
+	//safeWriteJump(hookAddr, codeCaveAddr.getUIntPtr());
+
+	//safeWrite16(codeCaveAddr.getUIntPtr(), 0xB848);  // mov rax
+	//safeWrite64(codeCaveAddr.getUIntPtr() + 0x2, (uintptr_t)SprintHandler_ProcessButton_IsDown_Hook);
+	//safeWrite16(codeCaveAddr.getUIntPtr() + 0xA, 0xD0FF);                       // call rax
+	//safeWriteJump(codeCaveAddr.getUIntPtr() + 0xC, (uintptr_t)hookAddr + 0x5);  // jmp
+
+	g_branchTrampoline.write5Call(hookAddr, (uintptr_t)SprintHandler_ProcessButton_IsDown_Hook);
+
 	return true;
 }
